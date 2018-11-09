bbbc189210086de6278f0a54bb11d40aa4f34fe0
Christina Williams <christinaxtwilliams@gmail.com>
2018-11-09 01:47:06 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,45 +1,55 @@
 class Artist
-  extend Concerns::Findable
+	extend Concerns::Findable
 
-  attr_accessor :name
-  attr_reader :songs
-  @@all = []
+	attr_accessor :name, :songs
+	@@all = []
 
-  def initialize(name)
-    @name = name
-    @songs = []
-  end
+	def initialize(name)
+		@name = name
+		self.songs = []
+		save
+	end
+	
+	def self.all
+		@@all
+	end
+	
+	def self.destroy_all
+		@@all = []
+	end
+	
+	def save
+		@@all << self
+	end
+	 
+	def self.create(name)
+		artist = Artist.new(name)
+		artist.save
+		artist
+	end
+	
+	def add_song(song)
+		# even though we could, we are NOT checking if current Artist has this song, such as:   self.songs.include?(song)
+		# instead, we are checking if the (song) has an artist property equal to this instance of Artist 'self' (current instance of Artist)
+		# if song.artist != self, we then set set.artist = self
 
-  def self.all
-    @@all
+    if song.artist != self
+      song.artist = self
+		end
+		
+		# THEN we check self.songs whether it includes this particular song yet
+			# we are associating the Song object (song) with and Artist object (self)
+ 
+    if !@songs.include?(song)
+      @songs << (song)
+    end
   end
-
-  def self.destroy_all
-    self.all.clear
-  end
-
-  def save
-    self.class.all << self
-  end
-
-  def self.create(name)
-    artist = Artist.new(name)
-    artist.save
-    artist
-  end
-  
-  def add_song(song)
-    song.artist == nil ? song.artist = self : nil
-    song.artist = self unless song.artist == self
-    self.songs.include?(song) || self.songs << song
-     above wouldn't pass tests
-    self.songs << song unless self.songs.include?(song)
-  end
-  
-  def genres
-    self.songs.map do |song|
-      song.genre
-    end.uniq
-  end
-
+ 
+	def genres
+		@genres = []
+		self.songs.each do |song|
+			@genres.push(song.genre)
+		end
+		@genres.uniq
+	end
 end
